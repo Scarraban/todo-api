a698e2afb7b6dd34bbb9c2cd88b0241a0d6442aa
Sam Butler-Thompson <sam@q2q-it.com>
2017-02-03 16:48:49 +0000

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// }
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+var grades = [25, 32, 65, 71]
+
+function addGrade(grade) {
+	grades.push(78);
+	debugger;
+	return grade;
+}
+
+console.log(addGrade(grades));
